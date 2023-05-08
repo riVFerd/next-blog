cca691e31c4198bc29af2eb859836f13aa2080ba
Virgy Ferdian Surya Firmansyah <virgyferdian213@gmail.com>
2023-05-08 21:51:11 +0700

    add progress bar indicator when navigation between pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,20 @@ import {Provider} from "react-redux";
 import React from "react";
 import store from "@/store/store";
 import Layout from "@/components/Layout";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.events.on("routeChangeStart", () => {
+    NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => {
+    NProgress.done();
+});
+
+Router.events.on("routeChangeError", () => {
+    NProgress.done();
+});
 
 export default function App({Component, pageProps}) {
 
