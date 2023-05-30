9f0d315f8dd4628c91239ea35ad62aad7282ebf6
Virgy Ferdian Surya Firmansyah <virgyferdian213@gmail.com>
2023-05-30 08:13:09 +0700

    add google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import store from "@/store/store";
 import Layout from "@/components/Layout";
 import Router from "next/router";
 import NProgress from "nprogress";
+import Script from "next/script";
 
 Router.events.on("routeChangeStart", () => {
     NProgress.start();
@@ -24,6 +25,22 @@ export default function App({Component, pageProps}) {
         <Provider store={store}>
             <Layout>
                 <Component {...pageProps} />
+                <Script
+                    async={true}
+                    id="analytics1"
+                    strategy="lazyOnload"
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+                >
+                </Script>
+                <Script id="analytics2" strategy="lazyOnload" async={true}>
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                    
+                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+                    `}
+                </Script>
             </Layout>
         </Provider>
     )
